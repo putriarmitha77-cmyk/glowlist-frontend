28ead1059167d765296fd54e595cc1ceff34afb2
putriarmitha77-cmyk <putriarmitha77@gmail.com>
2026-08-06 09:55:16 +0700

    Tambah beberapa file

---
+++ src/pages/Tentang.jsx
@@ -0,0 +1,24 @@
+export default function Tentang() {
+    return (
+        <div className="container py-5 px-5">
+            <div className="row text-start">
+                <div className="col-10">
+                    <h2 className="fw-bold mb-3">Tentang Saya </h2>
+                    <h4>Haii, Saya Andina Armitha </h4>
+
+                    <p>
+                        nama saya Andina, siswi kelas XII RPL 1. Saya memiliki keterkaitan pada dunia skincare karena
+                        saya senang mempelajari cara merawat kulit serta mengenal berbagai produk yang sesuai dengan
+                        kebutuhan kulit.
+                    </p>
+
+                    <p>
+                        Saya juga menyukai skincare karena dapat membantu menjaga kesehatan dan kebersihan kulit.
+                        Dengan merawat kulit secara rutin, saya merasa lebih percaya diri dan nyaman dalam menjalani
+                        aktivitas sehari-hari.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
